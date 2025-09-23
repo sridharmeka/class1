3730aba90710efd41ce7bb8f834baefff2d7454e
sridhar <mshridhar225@gmail.com>
2025-09-23 14:43:07 +0530

    hello world started

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash/
+
+persion1=$1
+persion2=$2
+
+echo "$persion1: is the king"
+echo "$persion2 : is the best king"
+echo "$persion1 is the best of the best"
+echo "$persion2 is the world best king"
