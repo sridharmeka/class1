eb23f9cdcc78a75dd1571a3f4191044d9c786929
sridhar <mshridhar225@gmail.com>
2025-09-23 14:29:24 +0530

    hello world started

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,5 +1,8 @@
 #!/bin/bash/
 
+persion1 = $persion1
+persion2 = $persion2
+
 echo "sridhar: is the king"
 echo "sri : is the best king"
 echo "sridhar is the best of the best"
---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash/
+
+persion1 = sridhar
+persion2 = sri
+
+echo "$persion1: is the king"
+echo "$persion2 : is the best king"
+echo "$persion1 is the best of the best"
+echo "$persion2 is the world best king"
