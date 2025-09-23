4fa00e5a7bcc85b4531cd2c23bb6bf41205e6d75
sridhar <mshridhar225@gmail.com>
2025-09-23 15:10:01 +0530

    hello world started

--- 06-data-types.sh
+++ 06-data-types.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-$number1=10
-$number2=20
+number1=100
+number2=500
 
 timestamp=$(date)
 echo "current timestamp: $timestamp"
+
+
 SUM=$((number1+number2))
 
 echo "the sum of $number1 and $number2 is : $sum"
