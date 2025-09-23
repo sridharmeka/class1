b26a4e5c659990ec0897dec0f4143b8f8f4e568a
sridhar <mshridhar225@gmail.com>
2025-09-23 14:52:59 +0530

    hello world started

---
+++ 05-variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "enter your pin number"
+read pin 
+if [ $pin -eq 1234 ]
+then 
+    echo "welcome sridhar"
+    else
+    echo "wrong pin"
+    fi
+    echo " thank you visit again"
+    
