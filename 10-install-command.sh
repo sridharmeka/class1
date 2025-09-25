18b160845c56d417872bbe6b74b96449bc0b7448
sridhar <mshridhar225@gmail.com>
2025-09-25 11:06:48 +0530

    specal vars

--- 10-install-command.sh
+++ 10-install-command.sh
@@ -5,6 +5,9 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then 
 echo "ERROR:: please run this script as root user or with sudo"
+exit 1 #give orther than 0 upto 127
+else
+echo "you are running with root access"
 fi
 
 dnf install mysql -y
