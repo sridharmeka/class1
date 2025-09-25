d7ea623eb43b6eb7cdf1c973e2b1014600463325
sridhar <mshridhar225@gmail.com>
2025-09-25 10:08:10 +0530

    specal vars

--- 08-specal-vars.sh
+++ 08-specal-vars.sh
@@ -2,3 +2,10 @@
 
 echo "all variables passed to the script: $@"
 echo "number of variables: $#"
+echo "script name: $0"
+echo "current directory: $PWD"
+echo "user running this script: $USER"
+echo "home directory of the user: $HOME"
+echo "process id of this script: $$"
+sleep 10 &
+echo "process id of lasdt command background: $!"
