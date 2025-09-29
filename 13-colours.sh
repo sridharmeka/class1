f457997385aaa59076346ffb33aff30fb8a1a0a1
sridhar <mshridhar225@gmail.com>
2025-09-29 15:44:18 +0530

    colours

---
+++ 13-colours.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+B="\e[34m"
+M="\e[35m"
+C="\e[36m"
+W="\e[37m"
+N="\e[0m"
+
+
+if [ $USERID -ne 0 ]
+then 
+echo "$R ERROR:: please run this script as root user or with sudo $N"
+exit 1 #give orther than 0 upto 127
+else
+echo "you are running with root access"
+fi
+
+VALIDATE() {
+if [ $1 -eq 0 ]
+then 
+echo " installing $2 is $G success $N"
+else
+echo "intalling $2 is $r failed $N"
+exit 1
+fi
+}
+
+dnf list installed mysql
+if [ $? -ne 0 ]
+then
+echo "mysql is not installed... $Y already installation $N we can proceed with installation"
+dnf install mysql -y
+VALIDATE $? "mysql"
+else
+echo -e "mysql is already installed... nothing to do "
+fi
+
+dnf list installed python3
+if [ $? -ne 0 ]
+then
+echo "python3 is not installed... we can proceed with installation"
+dnf install python3 -y
+VALIDATE $? "python3"
+else
+echo "python3 is already installed... nothing to do "
+fi
+
+dnf list installed nginx
+if [ $? -ne 0 ]
+then
+echo "nginx is not installed... we can proceed with installation"
+dnf install nginx -y
+VALIDATE $? "nginx"
+else
+echo "nginx is already installed... nothing to do "
+fi
+
+
+
+
+
+
